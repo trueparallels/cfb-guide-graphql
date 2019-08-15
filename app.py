602e5ec219f5f0427ce03a63d775bfafbaaee11d
Kyle Peacock <kyle.peacock@urbanelectric.com>
2019-08-15 09:16:25 -0400

    so far so good

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ app.debug = True
 app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=3003)
--- schema.py
+++ schema.py
@@ -1,14 +1,16 @@
-from graphene import ObjectType, String, Schema, Int, Decimal, Field, List
+from graphene import ObjectType, String, Schema, Int, Decimal, Field, List, Boolean
 import decimal
 import boto3
 from boto3.dynamodb.conditions import Attr, Key
+
+conferences_cache = {}
 
 class Conference(ObjectType):
     id = Decimal()
     name = String()
     parent_id = Decimal()
 
-class TeamsQuery(ObjectType):
+class ConferenceQuery(ObjectType):
     conference = List(Conference, id=Int(default_value=None))
 
     def resolve_conference(parent, info, **kwargs):
@@ -47,8 +49,11 @@ class Game(ObjectType):
     gameId = String()
     gameWeekYear = String()
     date = String()
+    home = String()
+    visitor = String()
     homeAbbreviation = String()
     visitorAbbreviation = String()
+    isNeutralSite = Boolean()
 
 class GamesQuery(ObjectType):
     byWeek = List(Game, week=String(default_value="2019-1"))
@@ -73,12 +78,77 @@ class GamesQuery(ObjectType):
                     date=item['date'],
                     homeAbbreviation=item['home_abbr'],
                     visitorAbbreviation=item['visitor_abbr'],
+                    home=item['home'],
+                    visitor=item['visitor'],
+                    isNeutralSite=item['neutral_site']
                 )
             )
 
         return games
 
-class AllQuery(TeamsQuery, GamesQuery, ObjectType):
+class Team(ObjectType):
+    id = Decimal()
+    abbreviation = String()
+    alternateColor = String()
+    color = String()
+    conference = Field(Conference)
+    displayName = String()
+    location = String()
+    name = String()
+
+    def resolve_conference(parent, info, **kwargs):
+        global conferences_cache
+
+        if len(conferences_cache):
+            print('int parent conf')
+            print(int(parent.conference))
+            return conferences_cache[int(parent.conference)]
+
+        db = boto3.resource('dynamodb')
+        leaguesTable = db.Table('cfb-guide-prod-leagues')
+
+        response = leaguesTable.scan(
+            Select='ALL_ATTRIBUTES'
+        )
+
+        for item in response['Items']:
+            conf_id = int(item['id'])
+            print('conf_id')
+            print(conf_id)
+            conferences_cache[conf_id] = item
+
+        return conferences_cache[int(parent.conference)]
+
+class TeamsQuery(ObjectType):
+    teams = List(Team)
+
+    def resolve_teams(parent, info, **kwargs):
+        teams = []
+
+        db = boto3.resource('dynamodb')
+        teamsTable = db.Table('cfb-guide-prod-teams')
+
+        response = teamsTable.scan(
+            Select='ALL_ATTRIBUTES'
+        )
+
+        for item in response['Items']:
+            teams.append(
+                Team(
+                    id=item['id'],
+                    abbreviation=item['abbreviation'],
+                    alternateColor=item['alternate_color'],
+                    color=item['color'],
+                    conference=item['conference'],
+                    displayName=item['display_name'],
+                    location=item['location'],
+                    name=item['name']
+                )
+            )
+
+        return teams
+
+class AllQuery(ConferenceQuery, GamesQuery, TeamsQuery, ObjectType):
     pass
 
 schema = Schema(query=AllQuery)
