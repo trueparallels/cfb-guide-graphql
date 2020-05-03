9e4af5884d99602ffa1c784d90ce9bb56f1aac37
Kyle Peacock <kyle.peacock@urbanelectric.com>
2020-05-03 16:42:43 -0400

    add long forgotten healthcheck endpoint for ALB

--- app/app.py
+++ app/app.py
@@ -8,5 +8,9 @@ app.debug = True
 
 app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
 
+@app.route('/ok')
+def ok():
+    return "ok"
+
 if __name__ == "__main__":
     serve(app, host='0.0.0.0', port=3003)
